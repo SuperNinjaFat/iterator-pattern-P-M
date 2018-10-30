d1e668efe5d85c77c36d3d27e5f5d62475cf839d
MatthewSchwartzkopf <matthew.schwartzkopf@mymail.champlain.edu>
2018-10-29 20:31:57 -0400

    Updating code

--- DinerMenu.java
+++ DinerMenu.java
@@ -3,14 +3,10 @@ public class DinerMenu {
 	static final int MAX_ITEMS = 6;
 	int numberOfItems = 0;
 	MenuItem[] menuItems;
-	
-	public Iterator createIterator() {
-		return new DinerMenuIterator(menuItems);
-	}
-	
+
 	public DinerMenu() {
 		menuItems = new MenuItem[MAX_ITEMS];
-		
+
 		addItem("Vegetarian BLT", "(Fakin') Bacon with lettuce & tomato on whole wheat", true, 2.99);
 	}
 
@@ -18,10 +14,13 @@ public class DinerMenu {
 		MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
 		if (numberOfItems >= MAX_ITEMS) {
 			System.err.println("Sorry, menu is full! Can't add item to menu");
-		}
-		else {
+		} else {
 			menuItems[numberOfItems] = menuItem;
 			numberOfItems = numberOfItems + 1;
 		}
+	}
+
+	public Iterator createIterator() {
+		return new DinerMenuIterator(menuItems);
 	}
 }
