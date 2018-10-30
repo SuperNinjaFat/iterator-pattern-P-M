0ff3b854dce88dfe8a4c5b0e21a39f147747a09f
MatthewSchwartzkopf <matthew.schwartzkopf@mymail.champlain.edu>
2018-10-29 20:33:22 -0400

    Updating code

--- PancakeHouseMenu.java
+++ PancakeHouseMenu.java
@@ -1,26 +1,27 @@
 import java.util.ArrayList;
 
 public class PancakeHouseMenu {
-	
-	ArrayList<MenuItem> menuItems;
-	
+	static final int MAX_ITEMS = 6;
+	int numberOfItems = 0;
+	MenuItem[] menuItems;
+
 	public PancakeHouseMenu() {
-		menuItems = new ArrayList<MenuItem>();
-		
+		menuItems = new MenuItem[MAX_ITEMS];
+
 		addItem("K&B's pancake Breakfast", "Pancakes with scrambled eggs, and toast", true, 2.99);
-		
 	}
-	public void addItem(String name, String description, boolean vegetarian, double price) {
+
+	private void addItem(String name, String description, boolean vegetarian, double price) {
 		MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
-		menuItems.add(menuItem);
-		
+		if (numberOfItems >= MAX_ITEMS) {
+			System.err.println("Sorry, menu is full! Can't add item to menu");
+		} else {
+			menuItems[numberOfItems] = menuItem;
+			numberOfItems = numberOfItems + 1;
+		}
 	}
-	
-	public ArrayList<MenuItem> getMenuItems() {
-		return menuItems;
-	}
-	
+
 	public Iterator createIterator() {
-		return null;
+		return new DinerMenuIterator(menuItems);
 	}
 }
