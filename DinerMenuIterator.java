f661408c3a6e461d033d2c960600793805f0eb79
MatthewSchwartzkopf <matthew.schwartzkopf@mymail.champlain.edu>
2018-10-29 20:32:14 -0400

    Updating code

--- DinerMenuIterator.java
+++ DinerMenuIterator.java
@@ -3,7 +3,7 @@ public class DinerMenuIterator implements Iterator {
 
 	MenuItem[] items;
 	int position = 0;
-	
+
 	public DinerMenuIterator(MenuItem[] menuItems) {
 		this.items = menuItems;
 	}
@@ -13,7 +13,7 @@ public class DinerMenuIterator implements Iterator {
 		position = position + 1;
 		return menuItem;
 	}
-	
+
 	public boolean hasNext() {
 		if (position >= items.length || items[position] == null)
 			return false;
